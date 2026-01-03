1e3e33d70b4ed7065d422e1c36ede4c2de5b3072
Reva Bhalodia <revabhalodia19@gmail.com>
2026-01-03 23:36:50 +0530

    feat: add practise questions

---
+++ day 3/practise.py
@@ -0,0 +1,31 @@
+"""mov = []
+movie1 = input("enter your first fav movie:")
+movie2 = input("enter your second fav movie:")
+movie3 = input("enter your third fav movie:")
+mov.append(movie1)
+mov.append(movie2)
+mov.append(movie3)
+print(mov)
+"""
+
+
+""""
+#panlindrome = starting and ending se kaise bhi padho its same eg = 12321
+list1 = [1,2,3,4,3,2,1]
+list2 = [1,2,3,4,5,6,7]
+copy_list1 = list1.copy()
+copy_list1.reverse()
+
+if(copy_list1 == list1):
+    print("palindrome")
+else:
+    print("not palindrome")
+    """
+#count students with grade a
+students = ("c","d","a","a","b","b","a")
+print(students.count("a"))
+
+#store the above values in list and sort them in ascending order
+students = ["c","d","a","a","b","b","a"]
+print(students.sort)
+print(students)
