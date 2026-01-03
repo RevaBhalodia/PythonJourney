9e7fd3c6f25e44903c66c3a8f5489666837c7d81
Reva Bhalodia <revabhalodia19@gmail.com>
2026-01-03 23:27:42 +0530

    feat: add list practice file

---
+++ day 3/list.py
@@ -0,0 +1,20 @@
+#list is a built in data type that stores set of values.
+
+marks1 = 24.3
+marks2 = 98.4
+marks3 = 78.5
+marks4 = 89.6
+marks5 = 92.7
+
+marks = [23.4, 78.9,90.0,34.5,87.9]
+print(marks)
+print(type(marks))
+print(len(marks))
+print(marks[0])
+print(marks[3])
+
+students = ["Rico", 20,8.17,"nagpur"]
+print(students[0])
+#list are mutable means it can change
+students[0] = ["nico"]
+print(students[0])
