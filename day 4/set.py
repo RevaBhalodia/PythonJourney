3832ad32968293a85a10c9f694a16139dbedea6d
Reva Bhalodia <revabhalodia19@gmail.com>
2026-01-04 17:18:55 +0530

    feat:add set

---
+++ day 4/set.py
@@ -0,0 +1,7 @@
+# set is a collection of umordered items, each elemts in the set must be unique and immutable
+collection = {1,3,5,7,9, "reva","bhalodia","reva","mike"} # repeated elements stored only once
+print(collection)
+print(type(collection))
+
+rico = set()
+print(type(rico))
