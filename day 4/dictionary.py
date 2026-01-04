63b5bca1ed348c0638c023afd0649e78f2562f00
Reva Bhalodia <revabhalodia19@gmail.com>
2026-01-04 17:15:05 +0530

    feat:add dictonary

---
+++ day 4/dictionary.py
@@ -0,0 +1,25 @@
+#dictionaries are used to store data values in key:value pairs
+# dict = {
+ #   name = rico
+#}
+info = {
+    "name" : "reva",
+    "age"  : 20,
+    "marks" : 98
+}
+
+print(info)
+
+#eg
+dict = {
+    "name" : ["rico","nico"],
+    "age" : [20,21],
+    "subjects" : ("cs","js","python"),
+    99.2 : "price"
+
+}
+print(dict)
+
+#empty/null dictonary
+null_dict = {}
+print(null_dict)
