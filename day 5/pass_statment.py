90aaf1158e71c88d504d073dd5367e850674409d
Reva Bhalodia <revabhalodia19@gmail.com>
2026-01-05 19:11:58 +0530

    feat:add pass_statment

---
+++ day 5/pass_statment.py
@@ -0,0 +1,11 @@
+for i in range(5):
+    pass
+
+print("doneeee")
+#pass is a null statment that does nothing , it is used as a placeholder for future code
+for i in range(10):
+    pass
+if i > 10:
+    pass
+
+print("done")
