d10d33a6c437092fb77e303868dfc331219a8ef5
Reva Bhalodia <revabhalodia19@gmail.com>
2026-01-05 19:08:44 +0530

    feat:add for_loops

---
+++ day 5/for_loops.py
@@ -0,0 +1,15 @@
+#for loops are used for sequential traversal
+list = [1,3,5,7,9]
+ 
+for val in list :
+    print(val)
+
+rico = ( 1, 2, 3,4,5,6,7)
+for num in rico:
+    print(num)
+
+friends =["nup","simmy","nico","bruno"]
+for i in friends:
+    print(i)
+else:
+    print("end")
