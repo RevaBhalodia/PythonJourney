c6dd091cc3a3493484e457a590ea9d1754dbb573
Reva Bhalodia <revabhalodia19@gmail.com>
2026-01-05 19:11:05 +0530

    feat:add loops

---
+++ day 5/loops.py
@@ -0,0 +1,19 @@
+# loops are used to repeat instructions
+# while loop
+'''
+count = 1
+while count <= 5:
+    print("hello")
+    count += 1
+    print (count)
+i = 1
+while count <= 5:
+    print("hello guys")
+    count += 2
+  '''
+i = 1
+while i <= 5:
+    print(i) 
+    i += 1
+
+print("done") 
