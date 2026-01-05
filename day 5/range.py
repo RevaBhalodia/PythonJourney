3d981f2398fe7b0aa3d7007f1665f48bab015f9a
Reva Bhalodia <revabhalodia19@gmail.com>
2026-01-05 19:13:06 +0530

    feat:add range

---
+++ day 5/range.py
@@ -0,0 +1,12 @@
+#range = a function returns a sequence of numbers,starting from 0 by default, and increase by 1, and stops before a specified number
+for r in range(6):
+    print(r)
+
+for n in range(1,9):
+    print(n)
+
+for rn in range(2, 10 ,2):
+    print(rn)
+
+for i in range(2, 101 , 2):
+    print(i)
