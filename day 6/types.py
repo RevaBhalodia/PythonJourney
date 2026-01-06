2c8ee8433baa1f6768ff16330f180a082a4d3f3e
Reva Bhalodia <revabhalodia19@gmail.com>
2026-01-06 18:32:18 +0530

    feat:add function types

---
+++ day 6/types.py
@@ -0,0 +1,11 @@
+#built-in functions 
+print("reva","bhalodia")
+list = [1,2,3,4,5,6]
+print(len(list))
+print(type(list))
+
+#user function
+def cal_prod(a=0,b=0):
+    print(a * b)
+    return a * b
+cal_prod()
