d1b96efee6e721819adae01f88fe1f012d9457c3
Reva Bhalodia <revabhalodia19@gmail.com>
2026-01-06 18:31:53 +0530

    feat:add recursive function

---
+++ day 6/recursion.py
@@ -0,0 +1,17 @@
+#recursion is a function which calls itself repeatedly
+def show(n):
+    if(n ==0):
+        return
+    print(n)
+    show(n-1)
+
+show(5)
+
+
+def fact(n):
+    if(n == 0 or n == 1):
+        return 1
+    else:
+        return n * fact(n-1)
+    
+print(fact(6))
