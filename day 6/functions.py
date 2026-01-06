518b89099cc8a0b0153e16b2771d9ac2363bdd77
Reva Bhalodia <revabhalodia19@gmail.com>
2026-01-06 18:30:09 +0530

    feat:add functions

---
+++ day 6/functions.py
@@ -0,0 +1,19 @@
+# function is a block of statements that performs a specific task
+def calc_sum(a,b):
+    sum = a + b
+    print(sum)
+    return sum
+calc_sum(5,10)
+calc_sum(2,9)
+calc_sum(5,3)
+
+
+#practise
+def cal_sum(a,b):  #parameters
+    return a + b
+sum = cal_sum(178,2) # function call; arguments
+print(sum)
+
+def print_hello():
+    print("hello")
+print_hello()
