48e5c8c03af28a8e3a84ccb2b05c47b0e5eb0176
Reva Bhalodia <revabhalodia19@gmail.com>
2026-01-07 19:10:14 +0530

    feat:add reading a file

---
+++ day 7/reading.py
@@ -0,0 +1,15 @@
+f = open(r"day 7\demo.txt", "r") 
+
+data = f.read(10)
+print(data)
+
+f.close()
+
+
+#line by line
+f = open(r"day 7\demo.txt", "r") 
+
+line1 = f.readline()
+print(line1)
+
+f.close()
