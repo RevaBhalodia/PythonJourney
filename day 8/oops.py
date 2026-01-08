467ae9222bdc692760d23635f87c362fd553a063
Reva Bhalodia <revabhalodia19@gmail.com>
2026-01-08 23:36:56 +0530

    feat:add opps in py

---
+++ day 8/oops.py
@@ -0,0 +1,21 @@
+# class = is a blueprint for creating an object
+"""
+class Student:
+    name = "eva wheeler"
+
+s1 = Student()
+print(s1.name)
+
+s2 = Student()
+print(s2.name)
+"""
+
+class car:
+    color = "black"
+    model = "99"
+    brand = "lamborgini"
+
+c1 = car()
+print(c1.color)
+print(c1.brand)
+
