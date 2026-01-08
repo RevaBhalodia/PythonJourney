df39556ac741f762a4bf9f193fb8e8dc90fc2a39
Reva Bhalodia <revabhalodia19@gmail.com>
2026-01-08 23:34:31 +0530

    feat:add abstraction in py

---
+++ day 8/abstraction.py
@@ -0,0 +1,14 @@
+#abstraction = hinding the enternal details and only showing what is necessary
+class car:
+    def __init__(self):
+        self.acc = False
+        self.brk = False
+        self.clutch = False
+
+    def start(self):
+        self.clutch = True
+        self.acc = True
+        print("car started..")
+
+car1 = car()
+car1.start()
