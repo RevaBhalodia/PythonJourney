813f39e13e33fcddc9c10d73f90f68f3c77e3b1c
Reva Bhalodia <revabhalodia19@gmail.com>
2026-01-11 18:53:34 +0530

    feat:add polymorphism in python

---
+++ day 10/polymorphism.py
@@ -0,0 +1,33 @@
+# polymorphism = when the same operator is allowed to have different meaning according to the context.
+#print(9 + 1)  # 10
+
+#print("rico" + "blasey")  #concatenate
+#print([1,2,3] + [4,5,6])  #merge
+
+# dunder functions = double underscore
+
+class Complex:
+    def __init__(self, real, img):
+        self.real = real
+        self.img = img
+
+    def showNumber(self):
+        print(self.real,"i +", self.img,"j")
+
+    def __add__(self, num2):
+        newReal = self.real + num2.real 
+        newImg = self.img + num2.img
+        return Complex(newReal, newImg )
+        
+       
+num1 = Complex(1,3) 
+num1.showNumber()
+
+num2 = Complex(2,4)
+num2.showNumber()
+
+num3 = num1 + num2
+num3.showNumber()
+
+#num3 = num1.add(num2)
+#num3.showNumber()
