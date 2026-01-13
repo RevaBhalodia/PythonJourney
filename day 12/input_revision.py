ec872016dd222085211c83661a0587481932ec7b
Reva Bhalodia <revabhalodia19@gmail.com>
2026-01-13 22:38:54 +0530

    feat: add revision of inputs and operators

---
+++ day 12/input_revision.py
@@ -0,0 +1,12 @@
+#Take the user’s name as input and print:hello<name>
+name = input("enter your name:")
+print("hello",name)
+
+
+#Take two numbers from the user and print their sum.
+a = int(input("enter the first number:"))
+b = int(input("enter the second number:"))
+
+sum = a + b
+
+print("sum of a and b is:",sum)
