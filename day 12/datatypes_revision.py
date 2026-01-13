5f9a997905fe678d53c9eae8c904ebce297057a3
Reva Bhalodia <revabhalodia19@gmail.com>
2026-01-13 22:37:37 +0530

    feat: add revision of datatypes

---
+++ day 12/datatypes_revision.py
@@ -0,0 +1,22 @@
+#Create variables of these types and print their type:Integer,Float,String,Boolean.
+name = "Rico"
+age = 20
+sgpa = 8.2
+is_twenty = (age == 20)
+
+print(is_twenty)
+
+print(type(name))
+print(type(age))
+print(type(sgpa))
+print(type(is_twenty))
+
+
+#Print the datatype of each variable using type() : x = 15,y = 3.5,z = "Python.
+x = 15
+y = 3.5
+z = "python"
+
+print(type(x))
+print(type(y))
+print(type(z))
