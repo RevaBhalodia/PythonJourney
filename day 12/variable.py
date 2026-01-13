578c6c9e78d7cbd4c3b5ed3d8631a26c30797499
Reva Bhalodia <revabhalodia19@gmail.com>
2026-01-13 22:39:49 +0530

    feat: add revision of variables

---
+++ day 12/variable.py
@@ -0,0 +1,14 @@
+#Create a variable age and store your age in it.Print a message like:My age is 20.
+age = 20
+print("my age is 20")
+print(type(age))
+
+
+#Create two variables a = 10 and b = 5.Swap their values and print the result.
+a = 10
+b = 5
+
+a,b = b,a
+
+print("a =",a)
+print("b =", b)
