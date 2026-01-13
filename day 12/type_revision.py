e688b2b1493ccb1cb7732cce430cf2c1e6cb8119
Reva Bhalodia <revabhalodia19@gmail.com>
2026-01-13 22:39:22 +0530

    feat: add revision of type conversion

---
+++ day 12/type_revision.py
@@ -0,0 +1,13 @@
+#Take a number as input from the user and convert it into an integer.Print its type before and after conversion.
+num = (input("enter any number:"))
+n = int(num)
+print(num)
+print(type(n))
+
+
+
+#Convert the following:a = "25",b = "10",Add them as numbers (not strings) and print the result.
+a = int("25")
+b = int("10")
+sum = a + b
+print(sum)
