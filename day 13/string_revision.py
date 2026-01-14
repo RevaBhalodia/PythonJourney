5764f998d6a612bfb3669e0b06f53436d609629b
Reva Bhalodia <revabhalodia19@gmail.com>
2026-01-14 21:39:33 +0530

    feat: add string revision

---
+++ day 13/string_revision.py
@@ -0,0 +1,12 @@
+#text = "hello world",Print:Length of the string,String in uppercase.
+str = "hello world"
+print(len(str))
+print(str.capitalize())
+
+
+#Take a string input from the user and print:First character,Last character.
+str = input("enter your string:")
+ch = str[0]
+print(ch)
+ch = str[-1]
+print(ch)
