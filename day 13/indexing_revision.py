7979dfa9d9b4332e5b068c18c0df10206fa99c0f
Reva Bhalodia <revabhalodia19@gmail.com>
2026-01-14 21:38:21 +0530

    feat: add indexing revision

---
+++ day 13/indexing_revision.py
@@ -0,0 +1,13 @@
+#Given:name = "Python",Print:First character,Last character.
+name = "python"
+ch = name[0]
+print(ch)
+ch = name[5]
+print(ch)
+
+#Given:numbers = [10, 20, 30, 40, 50],Print:The element at index 2,The element at index -1.
+numbers = [10, 20, 30, 40, 50]
+ch = numbers[2]
+print(ch)
+ch = numbers[-1]
+print(ch)
