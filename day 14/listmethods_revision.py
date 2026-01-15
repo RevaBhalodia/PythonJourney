26ef0f7fa5fb082f3834b5d13ed1fe542d24de15
Reva Bhalodia <revabhalodia19@gmail.com>
2026-01-15 22:23:11 +0530

    feat:add list methods revision

---
+++ day 14/listmethods_revision.py
@@ -0,0 +1,14 @@
+#Given:numbers = [10, 20, 30],Add 40 to the list,Remove 20 from the list,Print the updated list.
+list = [10,20,30]
+print(list.append(40))
+print(list)
+list.pop(1)
+print(list)
+
+
+#Create a list [5, 2, 8, 1] and Sort the list,Reverse the list,Print the final list.
+num = [5, 2, 8, 1]
+print(num.sort)
+print(num)
+print(num.sort(reverse=True))
+print(num)
