7f5fb385fae8cda4fc19734421e32e9f80813183
Reva Bhalodia <revabhalodia19@gmail.com>
2026-01-15 22:24:31 +0530

    feat:add tuple slicing revision

---
+++ day 14/tupleslicing_revision.py
@@ -0,0 +1,6 @@
+#Given:t = (5, 10, 15, 20, 25),Print:Elements from index 1 to 3,Last 2 elements.
+t = (5, 10, 15, 20, 25)
+print(t[1:4])
+print(t[3:5])
+#Print every alternate element from the tuple.
+print(t[::2])
