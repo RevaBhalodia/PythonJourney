99bce98ca64809f6028a8500f4fafa2aabe01ece
Reva Bhalodia <revabhalodia19@gmail.com>
2026-01-15 22:25:00 +0530

    feat:add tuple methods revision

---
+++ day 14/tuplesmethod_revision.py
@@ -0,0 +1,9 @@
+#Given:t = (1, 2, 3, 2, 4),Count how many times 2 appears,Find the index of 3.
+t = (1, 2, 3, 2,4)
+print(t.count(2))
+
+#Create a tuple of numbers and use:len(),max(),min().
+nums = (2,3,4,5,6,7,8,9)
+print(len(nums))
+print(max(nums))
+print(min(nums))
