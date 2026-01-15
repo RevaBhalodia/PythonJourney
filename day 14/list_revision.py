2492c7e0f8ad29722067bea19d75d08bebabdfda
Reva Bhalodia <revabhalodia19@gmail.com>
2026-01-15 22:22:48 +0530

    feat:add list revision

---
+++ day 14/list_revision.py
@@ -0,0 +1,12 @@
+#Create a list of 5 numbers and:Print the list,Print the first and last element.
+num = [1,2,3,4,5]
+print(num)
+print(num[0])
+print(num[-1])
+
+#Take 3 numbers from the user, store them in a list, and print the list.
+a = int(input("enter first number:"))
+b = int(input("enter second number:"))
+c = int(input("enter third number:"))
+list = [a,b,c]
+print(list)
