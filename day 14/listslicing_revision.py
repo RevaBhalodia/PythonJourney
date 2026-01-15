dff4ad1ab48156f79376dad5c3c1702e16efe1fa
Reva Bhalodia <revabhalodia19@gmail.com>
2026-01-15 22:23:40 +0530

    feat:add list slicing revision

---
+++ day 14/listslicing_revision.py
@@ -0,0 +1,7 @@
+#Given:numbers = [10, 20, 30, 40, 50, 60],Print:First 3 elements,Last 2 elements.
+numbers = [10, 20, 30, 40, 50, 60]
+print(numbers[0:3])
+print(numbers[4:])
+
+#Reverse the list using slicing.
+print(numbers.sort(reverse=True))
