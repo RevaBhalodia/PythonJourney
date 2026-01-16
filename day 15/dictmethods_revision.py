956a51230f87e8864f32d7fd6c30c370a7605c44
Reva Bhalodia <revabhalodia19@gmail.com>
2026-01-16 21:27:07 +0530

    feat: add dictionary methods revision

---
+++ day 15/dictmethods_revision.py
@@ -0,0 +1,8 @@
+#Q1.Given:marks = {"Math": 80, "Science": 75},
+#Add "English": 85,Update "Math" marks to 90,Print the updated dictionary.
+marks = {"Math": 80, "Science": 75}
+print(marks.update({"math": 80}))
+print(marks)
+#Q2.Using the same dictionary:print all keys,Print all values.
+print(marks.keys())
+print(marks.values())
