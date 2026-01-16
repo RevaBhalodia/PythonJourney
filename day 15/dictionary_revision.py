07ddff17f4467a494373dcc64b974a85b72c5401
Reva Bhalodia <revabhalodia19@gmail.com>
2026-01-16 21:26:40 +0530

    feat: add dictionary revision

---
+++ day 15/dictionary_revision.py
@@ -0,0 +1,13 @@
+#Q1.Create a dictionary to store:Name,Age,City,Print the dictionary.
+
+dict = {
+    "name": "rico",
+    "age": 20,
+    "city": "nagpur"
+    }
+print(dict)
+
+#Q2.Given:student = {"name": "Alex", "age": 20, "course": "IT"},Print:The value of "name",The value of "course".
+student = {"name": "Alex", "age": 20, "course": "IT"}
+print(student["name"])
+print(student["course"])
