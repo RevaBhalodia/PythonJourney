263a23e8eba0f340f20e70df03e3b2c16337ccdd
Reva Bhalodia <revabhalodia19@gmail.com>
2026-01-17 21:11:42 +0530

    feat: add pass statement revision

---
+++ day 16/passstatement_revision.py
@@ -0,0 +1,17 @@
+#Write a program where pass is used inside an if condition.
+for i in range(1,10):
+    if i % 2 == 0:
+        pass
+    else:
+        print(i)
+
+#Write a loop where pass is used and explain (in a comment) why it is used.
+
+for i in range(1,6):
+    if i == 3:    
+        pass    # does nothing , just acts as an placeholder.
+    else:
+        print(i)
+
+print("done")
+    
