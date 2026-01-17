8eb7d7cd2f6b6ecdc0f1eb66c0023192def4c395
Reva Bhalodia <revabhalodia19@gmail.com>
2026-01-17 21:10:24 +0530

    feat: add for loops revision

---
+++ day 16/forloop_revision.py
@@ -0,0 +1,9 @@
+#Use a for loop to print each character of the string "Python".
+str = "python"
+for i in str:
+    print(i)
+
+#Use a for loop to print all elements of the list [10, 20, 30, 40].
+num = [10, 20, 30, 40]
+for val in num:
+    print(val)
