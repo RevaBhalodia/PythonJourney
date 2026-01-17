214269a85fc1135f1db9875794324b478fc9c873
Reva Bhalodia <revabhalodia19@gmail.com>
2026-01-17 21:10:02 +0530

    feat: add loops revision

---
+++ day 16/loops_revision.py
@@ -0,0 +1,14 @@
+#Print numbers from 1 to 10 using a loop.
+
+i = 1
+while i <= 10:
+    print(i) 
+    i += 1
+
+#Print all even numbers between 1 and 20.
+i = 1
+while i <= 20:
+    if i % 2 == 0:
+        print(i)
+   
+    i += 1
