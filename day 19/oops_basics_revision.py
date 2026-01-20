12d65e6e184cb130e8edc7c0f3d9aa00890ea908
Reva Bhalodia <revabhalodia19@gmail.com>
2026-01-20 21:44:46 +0530

    feat:add oops basics revision

---
+++ day 19/oops_basics_revision.py
@@ -0,0 +1,18 @@
+#Create a class Student with attributes:name,age,Create an object and print the details.
+class Student:
+    name = "rico blasey"
+    age = 20
+
+s1 = Student()
+print(s1.name)
+print(s1.age)
+
+#Create a class Car with attributes:brand,model,Create two objects and print their details.
+class Car:
+    brand = "bmw"
+    model = "xseries"
+c1 = Car()
+print(c1.brand)
+
+c2 = Car()
+print(c2.model)
