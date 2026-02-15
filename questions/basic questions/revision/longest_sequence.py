890366eeb113361da05a502ded8d892d96bfcbe4
Reva Bhalodia <revabhalodia19@gmail.com>
2026-02-15 21:32:44 +0530

    feat:add revision questions

---
+++ questions/basic questions/revision/longest_sequence.py
@@ -0,0 +1,23 @@
+def longest_increasing_consecutive(nums):
+    if not nums:
+        return []
+
+    longest = []
+    current = [nums[0]]
+
+    for i in range(1, len(nums)):
+        if nums[i] == nums[i-1] + 1:
+            current.append(nums[i])
+        else:
+            if len(current) > len(longest):
+                longest = current
+            current = [nums[i]]
+
+
+    if len(current) > len(longest):
+        longest = current
+
+    return longest
+
+nums = [1, 2, 3, 1, 2, 3, 4, 1]
+print(longest_increasing_consecutive(nums))
