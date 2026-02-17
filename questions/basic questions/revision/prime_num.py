3204a8c1aba133113ac04612c0fa37e8254ef64a
Reva Bhalodia <revabhalodia19@gmail.com>
2026-02-17 19:46:10 +0530

    feat:add questions

---
+++ questions/basic questions/revision/prime_num.py
@@ -0,0 +1,22 @@
+def is_prime(num):
+    if num < 2:
+        return False
+    
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    
+    return True
+
+
+def sum_of_primes(n):
+    total = 0
+    
+    for i in range(2, n + 1):
+        if is_prime(i):
+            total += i
+    
+    return total
+
+
+print(sum_of_primes(10))
