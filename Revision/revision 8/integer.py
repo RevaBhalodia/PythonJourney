b77223f93670a3410624e092fd0c34c26473a077
Reva Bhalodia <revabhalodia19@gmail.com>
2026-02-19 23:11:30 +0530

    feat:add practise questions

---
+++ Revision/revision 8/integer.py
@@ -0,0 +1,13 @@
+def find_duplicates(nums):
+    seen = set()
+    duplicates = set()
+    
+    for num in nums:
+        if num in seen:
+            duplicates.add(num)
+        else:
+            seen.add(num)
+    
+    return list(duplicates)
+
+print(find_duplicates([1,2,3,2,4,1,5]))
