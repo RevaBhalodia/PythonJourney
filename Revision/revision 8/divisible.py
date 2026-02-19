648ccb9dab0e57b72bb6a760a21839c6831a23ac
Reva Bhalodia <revabhalodia19@gmail.com>
2026-02-19 23:11:18 +0530

    feat:add practise questions

---
+++ Revision/revision 8/divisible.py
@@ -0,0 +1,10 @@
+def sum_divisible(n):
+    total = 0
+    
+    for i in range(1, n + 1):
+        if i % 3 == 0 or i % 5 == 0:
+            total += i
+    
+    return total
+
+print(sum_divisible(10))
