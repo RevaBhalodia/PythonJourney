8d5d27f6ae1c683ed2b9e60e433ce8a8969ec0e2
Reva Bhalodia <revabhalodia19@gmail.com>
2026-02-19 23:11:06 +0530

    feat:add practise questions

---
+++ Revision/revision 8/Disarium_number.py
@@ -0,0 +1,14 @@
+num = int(input("Enter a number: "))
+
+temp = num
+digits = str(num)
+sum_value = 0
+
+for i in range(len(digits)):
+    digit = int(digits[i])
+    sum_value += digit ** (i + 1)
+
+if sum_value == num:
+    print(num, "is a Disarium number")
+else:
+    print(num, "is NOT a Disarium number")
