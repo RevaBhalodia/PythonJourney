e3bfc77c94384c4eb8033e384c1ce6d19bddc2cf
Reva Bhalodia <revabhalodia19@gmail.com>
2026-03-16 21:39:32 +0530

    feat:add practise revision

---
+++ questions/basic questions/questions/list_analysing.py
@@ -0,0 +1,10 @@
+numbers = [12, 45, 7, 23, 56, 34]
+
+largest = max(numbers)
+smallest = min(numbers)
+average = sum(numbers) / len(numbers)
+
+print("Numbers:", numbers)
+print("Largest:", largest)
+print("Smallest:", smallest)
+print("Average:", average)
