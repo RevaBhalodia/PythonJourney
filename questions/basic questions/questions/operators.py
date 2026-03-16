6a1b47f7717d47401fe3c7fac2e6a04802ec5ef1
Reva Bhalodia <revabhalodia19@gmail.com>
2026-03-16 21:40:01 +0530

    feat:add practise revision

---
+++ questions/basic questions/questions/operators.py
@@ -0,0 +1,8 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("Addition:", a + b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+
+print("Is first number greater than second?", a > b)
