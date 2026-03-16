a4805ab92366b05eccd0a38bb5c99bce41eda2a6
Reva Bhalodia <revabhalodia19@gmail.com>
2026-03-16 21:40:33 +0530

    feat:add practise revision

---
+++ questions/basic questions/questions/tuple_operations.py
@@ -0,0 +1,9 @@
+fruits = ("apple", "banana", "mango", "orange")
+
+print("First fruit:", fruits[0])
+print("Last fruit:", fruits[-1])
+
+fruit_list = list(fruits)
+
+print("Tuple:", fruits)
+print("Converted List:", fruit_list)
