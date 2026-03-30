132fcbdfc88028b58c283125fb6bbd559fd7c868
Reva Bhalodia <revabhalodia19@gmail.com>
2026-03-30 21:22:33 +0530

    feat:added revision questions on OOPs concepts in python

---
+++ projects/students_result_system.py
@@ -0,0 +1,25 @@
+class Student:
+    def __init__(self, name, marks):
+        self.name = name
+        self.marks = marks
+
+    def average(self):
+        return sum(self.marks) / len(self.marks)
+
+    def grade(self):
+        avg = self.average()
+
+        if avg >= 90:
+            return "A"
+        elif avg >= 75:
+            return "B"
+        elif avg >= 50:
+            return "C"
+        else:
+            return "Fail"
+
+
+s = Student("Rico", [80, 75, 90])
+
+print("Average:", s.average())
+print("Grade:", s.grade())
