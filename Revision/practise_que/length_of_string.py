7ccec565a9361bdef3b2439d39faa1c0e83bca4e
Reva Bhalodia <revabhalodia19@gmail.com>
2026-04-07 23:05:58 +0530

    feat:added practise questions

---
+++ Revision/practise_que/length_of_string.py
@@ -0,0 +1,13 @@
+def group_by_length(words):
+    result = {}
+
+    for word in words:
+        length = len(word)
+        if length not in result:
+            result[length] = []
+        result[length].append(word)
+
+    return result
+
+words = ["cat", "dog", "elephant", "bat", "car"]
+print(group_by_length(words))
