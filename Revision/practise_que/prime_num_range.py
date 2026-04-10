a4bac885caee3cb31b0b1e5872996a4b5fd791e8
Reva Bhalodia <revabhalodia19@gmail.com>
2026-04-10 21:54:34 +0530

    implementation of prime number in range

---
+++ Revision/practise_que/prime_num_range.py
@@ -0,0 +1,17 @@
+def is_prime(n):
+    if n < 2:
+        return False
+
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+
+    return True
+
+
+start = int(input("Start: "))
+end = int(input("End: "))
+
+for num in range(start, end + 1):
+    if is_prime(num):
+        print(num, end=" ")
