bae8887bb7e78c994430b0fe7321f0c2a2756416
Reva Bhalodia <revabhalodia19@gmail.com>
2026-04-12 22:21:15 +0530

    feat:added practise questions

---
+++ Revision/practise_que/login_system.py
@@ -0,0 +1,12 @@
+users = {
+    "admin": "1234",
+    "rico": "pass123"
+}
+
+username = input("Enter username: ")
+password = input("Enter password: ")
+
+if username in users and users[username] == password:
+    print("Login successful")
+else:
+    print("Invalid credentials")
