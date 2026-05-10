5cbbcdd7da10f2d14d4285d5a4bb77cd285934e4
Reva Bhalodia <revabhalodia19@gmail.com>
2026-05-10 21:34:49 +0530

    Found peak element using binary search

---
+++ Revision/diff questions/find_peak_element.py
@@ -0,0 +1,14 @@
+def find_peak(nums):
+    l, r = 0, len(nums)-1
+
+    while l < r:
+        mid = (l+r)//2
+
+        if nums[mid] > nums[mid+1]:
+            r = mid
+        else:
+            l = mid + 1
+
+    return l
+# Example
+print(find_peak([1, 2, 3, 1]))  # Output: 2
