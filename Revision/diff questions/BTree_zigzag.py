973c3395ce26898909905e1dcf97a54515b3079a
Reva Bhalodia <revabhalodia19@gmail.com>
2026-05-11 19:31:06 +0530

    Performed zigzag level order traversal of binary tree using BFS

---
+++ Revision/diff questions/BTree_zigzag.py
@@ -0,0 +1,32 @@
+from collections import deque
+
+def zigzag_level_order(root):
+    if not root:
+        return []
+
+    res = []
+    queue = deque([root])
+    left_to_right = True
+
+    while queue:
+        level = deque()
+
+        for _ in range(len(queue)):
+            node = queue.popleft()
+
+            if left_to_right:
+                level.append(node.val)
+            else:
+                level.appendleft(node.val)
+
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+
+        res.append(list(level))
+        left_to_right = not left_to_right
+
+    return res
+# Example usage:
+# Given a binary tree, return the zigzag level order traversal of its nodes' values.    
